f874e95c23d933fb93e00f30e1f2c2a5728393cb
Samin Azhan <34473594+Samin005@users.noreply.github.com>
2022-01-28 17:51:11 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Fllutter_TO_DO
+ A simple Flutter to-do app that allows users to create, delete and veiw a to-do list.
---
+++ android/app/src/main/kotlin/com/example/fllutter_to_do/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fllutter_to_do
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
